9089487a57a6fb3a5cd9f4fc8467279129476744
Trần Minh Thái <thaitranminh564@gmail.com>
2024-05-15 17:57:07 +0700

    daily forecast complete

--- script.js
+++ script.js
@@ -1,14 +1,5 @@
 const key = "b40f5664a7767d9c5a5384721dc13d5c"
 
-let cityName = "Ha Noi"
-let country = "VN"
-let weatherMain = "Clear"
-let currentTemp = 0
-let feelTemp = 0
-let humidity = 0
-let windSpeed = 0
-let timezone = 0
-
 let weatherNow = document.querySelector("#weather_main")
 let weatherIconNow = document.querySelector("#weather_icon")
 let temperatureNow = document.querySelector("#temperature")
@@ -16,68 +7,170 @@ let locationNow = document.querySelector("#location")
 
 
 function getWeather(city) {
+    (
+        async () => {
+            // get data weather now
+            const openWeatherAPINow = `https://api.openweathermap.org/data/2.5/weather?units=metric&q=${city}&appid=${key}`
+            const responseNow = await fetch(openWeatherAPINow)
+            const dataNow = await responseNow.json()
 
-    const openWeatherAPI = `https://api.openweathermap.org/data/2.5/weather?units=metric&q=${city}&appid=${key}`
+            // get data weather forecast
+            const openWeatherAPIForecast = `https://api.openweathermap.org/data/2.5/forecast?units=metric&q=${city}&appid=${key}`
+            const responseForecast = await fetch(openWeatherAPIForecast)
+            const dataForecast = await responseForecast.json()
 
-    fetch(openWeatherAPI)
-        .then(response => response.json())
-        .then(data => {
-            weatherMain = data.weather[0].main
-            cityName = data.name
-            country = data.sys.country
-            currentTemp = data.main.temp
-            feelTemp = data.main.feels_like
-            humidity = data.main.humidity
-            windSpeed = data.wind.speed
-            timezone = data.timezone
-            
-            console.log(data)
 
-            refresh()
-        })
-        .catch(err=>{
-            console.log(err)
-        })
+            refresh(dataNow, dataForecast)
+        }
+    )()
 }
 
 
-function refresh(){
+function refresh(dataNow, dataForecast) {
+    let time = getTimeInTimeZone(dataNow.timezone)
+
     //detail
-    weatherNow.textContent = weatherMain
-    weatherIconNow.src = `icon/${getIcon(weatherMain)}`
-    temperatureNow.textContent = currentTemp+'°'
-    locationNow.textContent = `${cityName}, ${country}`
+    weatherNow.textContent = dataNow.weather[0].main
+    weatherIconNow.src = `icon/${getIcon(dataNow.weather[0].main)}`
+    temperatureNow.textContent = Math.round(dataNow.main.temp) + '°'
+    locationNow.textContent = `${dataNow.name}, ${dataNow.sys.country}`
 
 
     //daily
-
+    refreshDailyHTML(getDailyWeather(time, dataForecast))
 }
 
+function refreshDailyHTML(dataDaily) {
+    let html = ``
+    for (let i in dataDaily) {
+        html += `
+        <div class="col-3">
+            <div class="row justify-content-center align-items-center my-3 mx-3 rounded-3 ${i == 0 ? 'today' : ''}">
+                <div class="col-5 d-flex justify-content-center align-items-center">
+                    <img src="icon/${getIcon(dataDaily[i].weather[0])}" alt="" height="60px">
+                </div>
+                <div class="col-7">
+                    <p class="text-background name-daily">${i == 0 ? 'Today' : dataDaily[i].day+'.'}</p>
+                        <span class="max_temp_daily text-background me-2">${dataDaily[i].max}°</span>
+                        <span class="min_temp_daily text-background">${dataDaily[i].min}°</span>
+                </div>
+            </div>
+        </div>
+        `
+    }
+    document.querySelector(".daily_nav").innerHTML = html
+}
 
-function getIcon(weather){
-    switch(weather){
-        case 'Thunderstorm':{
+function dailyObject(weather, day, max, min) {
+    this.weather = []
+    this.day = day
+    this.max = max
+    this.min = min
+}
+
+function getDailyWeather(time, dataForecast) {
+    let day = time.getDate()
+    let month = time.getMonth()
+    let timeDaiLy = []
+    let arrDaily = []
+    for (let data of dataForecast.list) {
+        let timeForecast = new Date(data.dt_txt).toDateString()
+        if (!timeDaiLy.includes(timeForecast)) {
+            timeDaiLy.push(timeForecast)
+            if (timeDaiLy.length >= 5) {
+                break;
+            } else {
+                arrDaily.push(new dailyObject())
+            }
+
+        }
+        if (timeForecast == timeDaiLy[timeDaiLy.length - 1]) {
+            if (arrDaily.length === 0) {
+                arrDaily.push(new dailyObject())
+            }
+            else {
+
+            }
+            let index = arrDaily.length - 1
+
+            arrDaily[index].day = timeForecast.substring(0,3)
+
+            arrDaily[index].weather.push(data.weather[0].main)
+
+            if (arrDaily[index].min > data.main.temp || arrDaily[index].min === undefined) arrDaily[index].min = Math.round(data.main.temp)
+
+            if (arrDaily[index].max < data.main.temp || arrDaily[index].max === undefined) arrDaily[index].max = Math.round(data.main.temp)
+
+            continue;
+        }
+    }
+    return arrDaily
+}
+
+function getTimeInTimeZone(offsetInSeconds) {
+    const localTime = new Date();
+
+    const offsetInMilliseconds = offsetInSeconds * 1000;
+
+    const utcTime = localTime.getTime() + (localTime.getTimezoneOffset() * 60000);
+    const targetTime = new Date(utcTime + offsetInMilliseconds);
+
+    return targetTime;
+}
+
+function getIcon(weather) {
+    switch (weather) {
+        case 'Thunderstorm': {
             return 'thunderstorm.png'
         }
-        case 'Drizzle':{
+        case 'Drizzle': {
             return 'drizzle.png'
         }
-        case 'Rain':{
+        case 'Rain': {
             return 'rain.png'
         }
-        case 'Snow':{
+        case 'Snow': {
             return 'snow.png'
         }
-        case 'Atmosphere':{
+        case 'Atmosphere': {
             return 'atmosphere.png'
         }
-        case 'Clear':{
+        case 'Clear': {
             return 'sun.png'
         }
-        case 'Clouds':{
+        case 'Clouds': {
             return 'cloud.png'
         }
     }
 }
 
-getWeather("ha noi")
+function getDay(day) {
+    switch (day) {
+        case 1: {
+            return 'Mon.'
+        }
+        case 2: {
+            return 'Tue.'
+        }
+        case 3: {
+            return 'Wed.'
+        }
+        case 4: {
+            return 'Thu.'
+        }
+        case 5: {
+            return 'Fri.'
+        }
+        case 6: {
+            return 'Sat.'
+        }
+        case 7: {
+            return 'Sun.'
+        }
+        default: {
+            return 'Bug!!!'
+        }
+    }
+}
+
+getWeather("New york")
